0d3f67a87aae5aca0e4876ca8611f130e8360d3a
VINAY5277 <vinaybandgar52@gmail.com>
2024-09-24 04:37:30 +0530

    add the files

---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,15 @@
+package sept.ex_06092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        System.out.println("Hi, sir");
+        // this code will not be executed.
+        // this is used to show the user or dev instructions
+        // you can write anything here
+        /*
+         *
+         *
+         *
+         */
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,13 @@
+//below code is package : with the sep folder
+package sept.ex_06092024;
+/* multi line comment
+ *
+ * write anything here
+ *
+ */
+public class Lab008 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+}
